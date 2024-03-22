97a63d6042a260c4cf56e0def4a660742607dda6
Medo20300 <medhatconcrete@gmail.com>
2024-03-22 07:01:07 -0700

    g

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Install puppet-lint
-package { 'puppet-lint':
-ensure   => '5.5.10',
-provider => 'gem'
+#Install flask version 2.1.0
+
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }
