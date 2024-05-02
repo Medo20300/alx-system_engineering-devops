a879f5085c921dd5e1ba1e7b96dc258a9813ca87
Medo20300 <medhatconcrete@gmail.com>
2024-05-02 09:25:58 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,27 @@
-# Automating project requirements using Puppe
+inifest for automating project requirements using Puppet
 
+# Ensure nginx package is installed
 package { 'nginx':
   ensure => installed,
 }
 
+# Add a line to the nginx configuration file for redirection
 file_line { 'install':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.github.com/Medo20300 permanent;',
 }
 
+# Create a file with Hello World! content
 file { '/var/www/html/index.html':
+  ensure  => present,
   content => 'Hello World!',
 }
 
+# Ensure nginx service is running and requires nginx package
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
+i
